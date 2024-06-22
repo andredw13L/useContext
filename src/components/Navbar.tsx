bd876079b6e35bc070e59bf36f0ea6b5ce07ec63
andredw13L <andreribeiro13l@gmail.com>
2024-06-22 17:02:15 -0300

    Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom"
+
+const Navbar = () => {
+  return (
+    <nav>
+        <ul>
+            <li>
+                <Link to="/">Home</Link>
+            </li>
+        </ul>
+        <ul>
+            <li>
+                <Link to="/contact">Contato</Link>
+            </li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
