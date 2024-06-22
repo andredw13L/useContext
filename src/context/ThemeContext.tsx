6f7ddc117fe5d5824c351028d01f06bed5aeeac5
andredw13L <andreribeiro13l@gmail.com>
2024-06-22 17:15:33 -0300

    context criado

---
+++ src/context/ThemeContext.tsx
@@ -0,0 +1,19 @@
+import { createContext, useState } from "react";
+
+export const ThemeContext = createContext()
+
+
+export const ThemeProvider = ({children}) => {
+
+    const [theme, setTheme] = useState("dark")
+
+    const toogleTheme = () => {
+        setTheme(theme === "light" ? "dark" : "light")
+    }
+
+    return (
+        <ThemeContext.Provider value={{theme, toogleTheme}}>
+         {children}
+        </ThemeContext.Provider>
+    )
+}
