45b68ecfb4d316d3aeb4058a8c637cc28cc93109
andredw13L <andreribeiro13l@gmail.com>
2024-06-22 17:44:53 -0300

    Context adicionado ao contact

--- src/routes/Contact.tsx
+++ src/routes/Contact.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { useContext } from 'react'
+import { ThemeContext } from '../context/ThemeContext'
 
 const Contact = () => {
+
+  const { theme } = useContext(ThemeContext)
+
   return (
-    <div>Contact</div>
+    <>
+      <h1>Página de contato</h1>
+      <p>O tema atual é: {theme}</p>
+    </>
   )
 }
 
